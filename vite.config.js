d520cdefa5615327a229b304aee779287fa066ae
Dio Arayan <drarayan1994@gmail.com>
2023-08-03 15:33:42 +0800

    revert base to /

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
   ],
-  base: '/dio-space/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
